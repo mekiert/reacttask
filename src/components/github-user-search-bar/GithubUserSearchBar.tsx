e965168f8a41418c0b4727fce156016663ac686f
Mateusz Ekiert <mateki970@wp.pl>
2021-08-04 23:34:37 +0200

    Styling in GithubUserSearchBar

--- src/components/github-user-search-bar/GithubUserSearchBar.tsx
+++ src/components/github-user-search-bar/GithubUserSearchBar.tsx
@@ -1,6 +1,7 @@
 import React, {ReactElement, useState} from "react";
 import styles from './GithubUserSearchBar.module.scss'
 import i18n from '../../i18n/i18n'
+import {ReactComponent as SearchIcon} from "../../resources/SearchIcon.svg";
 
 type Props = {
     onSearch: (userName: string) => void
@@ -15,8 +16,11 @@ export default function GithubUserSearchBar(props: Props): ReactElement {
 
     return (
         <div className={styles.wrapper}>
-            <input className={styles.searchField} placeholder={i18n.searchGithubUsers}
-                   value={usernameToFind} onInput={onUsernameUpdate}/>
+            <div className={styles.searchFieldWrapper}>
+                <SearchIcon className={styles.searchFieldIcon}/>
+                <input className={styles.searchField} placeholder={i18n.searchGithubUsers}
+                       value={usernameToFind} onInput={onUsernameUpdate}/>
+            </div>
             <button className={styles.searchButton}
                     onClick={() => props.onSearch(usernameToFind)}>{i18n.search}</button>
         </div>
