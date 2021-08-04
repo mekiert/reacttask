81e9bf0ae70516df8dbb7cca13d489ee170711ce
Mateusz Ekiert <mateki970@wp.pl>
2021-08-04 20:31:02 +0200

    Working GithubUserSearchBar with styles (without search icon)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React, {ReactElement} from 'react';
 import GithubUserSearchBar from "./components/github-user-search-bar/GithubUserSearchBar";
 
 export default function App(): ReactElement {
-  return (
-    <div>
-      <GithubUserSearchBar onSearch={(userName => console.log(userName))}/>
-    </div>
-  );
+    const findUsers = (username: string) => {
+        console.log(username);
+    }
+
+    return (
+        <div>
+            <GithubUserSearchBar onSearch={findUsers}/>
+        </div>
+    );
 }
--- src/components/github-user-search-bar/GithubUserSearchBar.tsx
+++ src/components/github-user-search-bar/GithubUserSearchBar.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useState} from "react";
 import styles from './GithubUserSearchBar.module.scss'
 import i18n from '../../i18n/i18n'
 
@@ -7,10 +7,18 @@ type Props = {
 }
 
 export default function GithubUserSearchBar(props: Props): ReactElement {
+    const [usernameToFind, changeUsername] = useState('');
+
+    const onUsernameUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+        changeUsername(event.target.value);
+    }
+
     return (
-        <div className={styles.searchField}>
-            <input/>
-            <button onClick={() => props.onSearch('abc')}>{i18n.search}</button>
+        <div className={styles.wrapper}>
+            <input className={styles.searchField} placeholder={i18n.searchGithubUsers}
+                   value={usernameToFind} onInput={onUsernameUpdate}/>
+            <button className={styles.searchButton}
+                    onClick={() => props.onSearch(usernameToFind)}>{i18n.search}</button>
         </div>
     )
 }
